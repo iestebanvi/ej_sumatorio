7b5eed5b810e65d9d9432ce841d997d01620e797
Ismael Esteban <ismael.esteban@t-systems.com>
2022-07-11 10:01:47 +0200

    commit

--- sum_lt_10/main.py
+++ sum_lt_10/main.py
@@ -1,34 +1,47 @@
 #!/bin/python3
 import random
 
-def check_sum (num1,num2,result):
-  resultado=num1+num2
-  print (resultado)
-
-  if (num1+num2) == result:
+def check_sum (num1,num2,inputVal):
+  if (num1+num2) == inputVal:
     return True
   else:
     return False
 
 inputVal=''
+npoints=0
+nfails=0
+
 
 while inputVal != 'quit':
   num1=random.randint(0, 12)
   num2=random.randint(0, 12)
-  print (f""" Cuanto suman los numero
+
+  print (f"""Para salir, escribir 'quit'. 
+  Cuanto suman los numeros
     {num1}
   +
     {num2}
   ----------
   """)
-  try:
-    result=int(input())
-    if check_sum (num1,num2,result):
-      print (True)
-    else:
-      print (False)
-  except:
-    print ("Error en el numero introducido")
-    if result == "quit":
-      exit()  
+  
+  inputVal=input()
+
+  if inputVal == 'quit':
+    print ("Hasta Luego...!!")
+    print ("")
+    exit()
+  else:
+    try:
+      inputVal=int(inputVal)
+      if check_sum (num1,num2,inputVal):
+        print ("Correcto!!")
+        npoints+=1
+        print (f"Puntos acumulados: {npoints}")
+        print (f"ERRORES: {nfails}")
+      else:
+        print (f"Ohh, fallo. La suma son {num1+num2} Puntos acumulados: {npoints}")   
+        nfails+=1
+    except:
+      print ("Error en el numero introducido")
+
 
